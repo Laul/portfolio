ffd3f56d549c2cbab110b9b08f6524fb52c2c596
Lauranne Sins <lauranne.sins@gmail.com>
2024-11-11 13:57:53 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Lauranne Sins - Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
